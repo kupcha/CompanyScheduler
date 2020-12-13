54a4858deef1bff92d6643b7175ad8c67532864d
Kyle VanWageninge <73082272+Kylevanw@users.noreply.github.com>
2020-12-12 23:22:04 -0500

    Update Request.java

--- Company/src/Request.java
+++ Company/src/Request.java
@@ -10,7 +10,13 @@ public class Request {
 	Date end;
 	int prevAvail;
 	int dayNumber = -1;
-	
+	/*
+	 * Author: Kyle VanWageninge
+	 * 
+	 * Constructor
+	 * 
+	 * @params: Username of employee
+	 */
 	public Request(String user, Date weekOf, Date start, Date end, int prevAvail)
 	{
 		
@@ -22,23 +28,34 @@ public class Request {
 		
 		
 	}
+	/*
+	 * @returns: number the day falls on in regards to the weekOf 
+	 */
 	public int getDay()
 	{
 		return dayNumber;
 	}
+	/*
+	 * @returns:Get the prevAvail var
+	 */
 	public int getPrev()
 	{
 		
 		return prevAvail;
 		
 	}
-	
+	/*
+	 * sets the diff of the dayNumber
+	 */
 	public void setDiff(int diff)
 	{
 		
 		dayNumber = diff;
 		
 	}
+	/*
+	 * Sets the prevAvail back in the database
+	 */
 	public void setPrevAvail() throws ClassNotFoundException, SQLException
 	{
 		
@@ -48,23 +65,34 @@ public class Request {
 		
 		
 	}
-
+	/*
+	 * Gets the weekOf for the Request
+	 */
 	public Date getWeekOf()
 	{
 		
 		return weekOf;
 		
 	}
+	/*
+	 * @returns: start date of the request
+	 */
 	public Date getStart()
 	{
 		
 		return start;
 		
 	}
+	/*
+	 * @returns: The end date of the request
+	 */
 	public Date getEnd()
 	{
 		return end;
 	}
+	/*
+	 * @returns: user making the request
+	 */
 	public String getUser()
 	{
 		return user;
